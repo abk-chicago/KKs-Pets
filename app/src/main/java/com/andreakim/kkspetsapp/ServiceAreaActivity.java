311f0419ba827399c12dc03cb02017baa1148884
Andrea Kim <andreaforanimals@gmail.com>
2016-08-08 16:47:18 -0500

    up to 4 activities
    Login, Main, Schedule, Service Area

---
+++ app/src/main/java/com/andreakim/kkspetsapp/ServiceAreaActivity.java
@@ -0,0 +1,40 @@
+package com.andreakim.kkspetsapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ServiceAreaActivity extends AppCompatActivity {
+
+    public void composeEmail() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:andreaforanimals@gmail.com")); // only email apps should handle this
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_area);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                composeEmail();
+                Snackbar.make(view, "Email KK's Pet Sitting now", Snackbar.LENGTH_LONG)
+                        .setAction("Email", null).show();
+
+            }
+        });
+    }
+
+}
