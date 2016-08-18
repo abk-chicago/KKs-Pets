c6b1024c86ea9ad0da4093b9987d263326983ecf
Andrea Kim <andreaforanimals@gmail.com>
2016-08-18 14:14:59 -0500

    fixed some big things

--- app/src/androidTest/java/com/andreakim/kkspetschicago/ApplicationTest.java
+++ app/src/androidTest/java/com/andreakim/kkspetschicago/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.andreakim.kkspetsapp;
+package com.andreakim.kkspetschicago;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/andreakim/kkspetschicago/MainActivity.java
+++ app/src/main/java/com/andreakim/kkspetschicago/MainActivity.java
@@ -1,25 +1,20 @@
-package com.andreakim.kkspetsapp;
+package com.andreakim.kkspetschicago;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.squareup.picasso.Picasso;
 
